1c64871e1dde63b082399e143977c2932cf4a458
unknown <mattschembri@gmail.com>
2020-10-10 17:19:32 +1100

    hange icons and add current user

--- src/Header/HeaderRight.js
+++ src/Header/HeaderRight.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 const HeaderRight = (props) => {
   return (
-    <div className="col" >
-        <div class="flex-grid header-right">
-            <img className=" col thumb-comment" src={require(`../images/profile.jpg`)}></img>
-            <img className=" col thumb-comment" src={require(`../images/profile.jpg`)}></img>
-            <img className=" col thumb-comment" src={require(`../images/profile.jpg`)}></img>
-            <img className=" col thumb-comment" src={require(`../images/profile.jpg`)}></img>
+    <div className="col flex-grid align-centre end">
+        <div class="flex-grid header-right align-centre">  
+            <div className="flex-grid align-centre">
+              <div className="current-user flex-grid align-centre">
+                <img className="col thumb-comment" src={require(`../images/profile.jpg`)}></img>
+                <div className="col">Matthew</div>
+              </div>
+              <div className="icon-circle"><i class="las la-plus"></i></div>
+              <div className="icon-circle"><i class="las la-comment-alt"></i></div>
+              <div className="icon-circle"><i class="las la-bell"></i></div>
+              <div className="icon-circle"><i class="las la-chevron-down"></i></div>
+            </div>
         </div>
     </div>
   );
